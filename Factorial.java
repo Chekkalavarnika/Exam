6efbdaa0b94cecbb7231b5ae60145d7f98e99a92
varnika <141485243+Chekkalavarnika@users.noreply.github.com>
2026-09-10 14:50:51 +0530

    Refactor Factorial class and method formatting

--- Factorial.java
+++ Factorial.java
@@ -1,13 +1,19 @@
-public class factorial{
-    static long factorial(int n){
-        long result=1;
-        for(int i=1;i<=n;i++){
-            result=result*i;
+public class Factorial {
+
+    static long factorial(int n) {
+        long result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
         }
+
         return result;
     }
-    public static void main(String[] args){
-        int number=7;
-        System.out.println("Factorial of"+ number +"is"+factorial(number));
+
+    public static void main(String[] args) {
+
+        int number = 7;
+
+        System.out.println("Factorial of " + number + " is " + factorial(number));
     }
 }
