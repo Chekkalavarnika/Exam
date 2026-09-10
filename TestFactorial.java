0efc6768c994fb89f24128f198b4138548d49e88
varnika <141485243+Chekkalavarnika@users.noreply.github.com>
2026-09-10 14:50:14 +0530

    Update TestFactorial.java

--- TestFactorial.java
+++ TestFactorial.java
@@ -1,15 +1,20 @@
-public class TestFactorial{
-    public static void main(String[] args){
-        try{
-            if(Factorial.compute(5)!=125){
+public class TestFactorial {
+
+    public static void main(String[] args) {
+
+        try {
+            if (Factorial.compute(5) != 120) {
                 throw new AssertionError("Test failed for 5");
             }
-            if(Factorial.compute(0)!=1){
+
+            if (Factorial.compute(0) != 1) {
                 throw new AssertionError("Test failed for 0");
             }
-            System.out.println("All tests are passed");
+
+            System.out.println("All tests passed.");
+
+        } catch (Exception e) {
+            System.out.println("Test failed: " + e.getMessage());
         }
-    }catch(Exception e){
-        System.out.println("Tests are failed"+e.getMessage());
     }
 }
